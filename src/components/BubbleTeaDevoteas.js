9e72feb0dfe07cd8b8eb0fc0a33faff2fb8f744d
OoL33 <alanalee@tutanota.com>
2023-08-03 13:55:52 -0400

    Adjusting layout and CSS

--- src/components/BubbleTeaDevoteas.js
+++ src/components/BubbleTeaDevoteas.js
@@ -5,16 +5,26 @@ import FooterForPost from "./FooterForPost";
 
 const BubbleTeaDevoteas = () => {
   return (
-    <div id="content">
-      <div className="post">
-        <div className="copy">
+    <div className="post">
+      <div className="copy">
         <div id="header">Bubble Tea Devoteas</div>
         <div className="image">
-        <img src={bubble_tea_front_page} alt="Bubble Tea Devoteas Front Page" />
-        <img src={bubble_tea_review_page} alt="Bubble Tea Devoteas Review Page" />
+          <img
+            src={bubble_tea_front_page}
+            alt="Bubble Tea Devoteas front page"
+          />
+          <img
+            src={bubble_tea_review_page}
+            alt="Bubble Tea Devoteas review page on one shop"
+          />
         </div>
-        <p className="description">In this app, built with Ruby on Rails, React, and Postgresql; users are able to see shops and reviews for bubble tea shops around Boston, MA. Signed in users can create reviews, and admin roles can create shops.</p>
-        </div>
+        <p className="description">
+          An application that allows users to se bubble tea shops and their
+          reviews around Boston, MA. Signed in users can create reviews, and
+          admin roles can create shops.
+        </p>
+      </div>
+      <div>
         <FooterForPost />
       </div>
     </div>
---
+++ src/components/MTGDecks.js
@@ -0,0 +1,30 @@
+import React from "react";
+import mtg_deck_frontpage from "/public/images/mtg_decks/mtg_deck_frontpage.png";
+import mtg_deck_deckpage from "/public/images/mtg_decks/mtg_deck_deckpage.png";
+import FooterForPost from "./FooterForPost";
+
+const MTGDecks = () => {
+  return (
+    <div className="post">
+      <div className="copy">
+        <div id="header">MTG Deck Builder</div>
+        <div className="image">
+          <img src={mtg_deck_frontpage} alt="MTG Deck Builder front page" />
+          <img
+            src={mtg_deck_deckpage}
+            alt="MTG Deck Builder page for one deck containing a card"
+          />
+        </div>
+        <p className="description">
+          An application that allows you to store and manage your Magic The
+          Gathering deck builds.
+        </p>
+      </div>
+      <div>
+        <FooterForPost />
+      </div>
+    </div>
+  );
+};
+
+export default MTGDecks;
